a9942db965f411eb28103e63f96e2791d92e650d
paulodallago <paulohdallago@gmail.com>
2023-07-19 00:41:16 -0300

    projects split

--- React/AppForma/components/Constants.js
+++
@@ -1,9 +0,0 @@
-import {Dimensions} from 'react-native';
-
-const vh = (Dimensions.get('window').height)/100;
-const vw = (Dimensions.get('window').width)/100;
-const bgColor = 'white';
-const lgBlue = '#28b2b4';
-const dkBlue = '#07374b';
-
-export {vh, vw, bgColor, lgBlue, dkBlue}; 
