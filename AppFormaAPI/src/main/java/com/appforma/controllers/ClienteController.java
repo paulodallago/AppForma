e67083a01322b6b386049b22b94b370c4e023b55
renanpolese <103063973+renanpolese@users.noreply.github.com>
2023-08-02 10:44:02 -0300

    ClienteController completo

--- AppFormaAPI/src/main/java/com/appforma/controllers/ClienteController.java
+++ AppFormaAPI/src/main/java/com/appforma/controllers/ClienteController.java
@@ -3,9 +3,15 @@ package com.appforma.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appforma.DTOs.Cliente.ClienteRequestDTO;
@@ -13,8 +19,9 @@ import com.appforma.DTOs.Cliente.ClienteResponseDTO;
 import com.appforma.entities.Cliente;
 import com.appforma.repositories.ClienteRepository;
 
+
 @RestController
-//se possivel fazer essa classe extender um GenericController
+@RequestMapping("/cliente")
 public class ClienteController {
 
     /* @Autowired
@@ -23,7 +30,7 @@ public class ClienteController {
     @Autowired 
     private ClienteRepository repository;
 
-    @GetMapping("/cliente")   
+    @GetMapping  
       public List<ClienteResponseDTO> getCliente() {
         List<ClienteResponseDTO> listaClientes = repository.findAll() //Armazena lista de todos os objetos na variavel listaClientes
         .stream() //Não entendi direito oq faz mas funciona
@@ -32,10 +39,37 @@ public class ClienteController {
         return listaClientes;
     }
 
-    @PostMapping("/cliente")
-    public void salvarCliente(@RequestBody ClienteRequestDTO data){
-        Cliente clienteData = new Cliente(data);
-        repository.save(clienteData);
+    @PostMapping
+    public ResponseEntity<Object> salvarCliente(@RequestBody ClienteRequestDTO data){
+        try{
+            Cliente clienteData = new Cliente(data);
+            repository.save(clienteData);
+            return ResponseEntity.status(HttpStatus.OK).body(clienteData);
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
-    
+
+    @PutMapping("/{id}")
+	public ResponseEntity<Object> updateCliente(@PathVariable Integer id, @RequestBody ClienteRequestDTO data) {
+		try {
+            Cliente clienteData = new Cliente(data); //Instancia uma classe Cliente passando os parâmetros do corpo da requisicao JSON
+			clienteData.setId(id); //Define o ID passado na URL como identificador unico do objeto que foi alterado
+			repository.save(clienteData); //Salva no banco de dados
+			return ResponseEntity.status(HttpStatus.OK).body(data);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCliente(@PathVariable Integer id){
+        try{
+            repository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body("ID "+id+" deletado");
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }        
+    }
+
 }
